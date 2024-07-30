36c6047520d47797a6d7e380f05c6e85368aaaa1
4m4z1gh <aitbaha48@gmail.com>
2024-07-30 16:36:52 +0300

    --

--- 1-Array/static.go
+++ 1-Array/static.go
@@ -1,58 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-var (
-	InvalidIndexError = errors.New("Invalid Index")
-)
-
-var staticArray []int
-
-func Remove(index int) error {
-	if index < 0 || index >= len(staticArray) {
-		return InvalidIndexError
-	}
-	staticArray = append(staticArray[:index], staticArray[index+1:]...)
-	return nil
-}
-
-func Add(item int) {
-	staticArray = append(staticArray, item)
-}
-
-func GetAt(index int) (int, error) {
-	if index < 0 || index >= len(staticArray) {
-		return -1, InvalidIndexError
-	}
-	return staticArray[index], nil
-}
-
-func Update(index int, item int) error {
-	if index < 0 || index >= len(staticArray) {
-		return InvalidIndexError
-	}
-	staticArray[index] = item
-	return nil
-}
+var staticArray [5]int
 
 func main() {
-	Add(1)
-	Add(1)
-	Add(1)
-	Add(1)
-	Add(3)
-	err := Remove(4)
-	if err != nil {
-		panic(err.Error())
-	}
-	value, err := GetAt(3)
-	if err != nil {
-		panic(err.Error())
-	}
-	err = Update(3, 4)
+	staticArray[0] = 1
+	staticArray[1] = 22
+	staticArray[2] = 5
+	staticArray[3] = 4
+	staticArray[4] = 3
 
-	fmt.Println(staticArray, value)
+	staticArray[2] = 4
+	fmt.Println(staticArray)
 }
