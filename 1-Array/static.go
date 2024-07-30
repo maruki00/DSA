c3018d1ec4429b09c8adfbbceb849d227ea99b4c
4m4z1gh <aitbaha48@gmail.com>
2024-07-30 16:29:41 +0300

    --

--- 1-Array/static.go
+++ 1-Array/static.go
@@ -1,19 +1,48 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	InvalidIndexError = errors.New("Invalid Index")
+)
+
 var staticArray []int
 
-func remove(index int) {
-	staticArray = append(staticArray[:index], staticArray[index:]...)
+func remove(index int) error {
+	if index < 0 || index >= len(staticArray) {
+		return InvalidIndexError
+	}
+	staticArray = append(staticArray[:index], staticArray[index+1:]...)
+	return nil
 }
 
 func add(item int) {
 	staticArray = append(staticArray, item)
 }
 
-func getAt(index int) int {
-	return staticArray[index]
+func getAt(index int) (int, error) {
+	if index < 0 || index >= len(staticArray) {
+		return -1, InvalidIndexError
+	}
+	return staticArray[index], nil
+}
+
+func Update(index int, item int) error {
+	if index < 0 || index >= len(staticArray) {
+		return InvalidIndexError
+	}
+	staticArray[index] = item
+	return nil
 }
 
 func main() {
-
+	add(1)
+	add(1)
+	add(1)
+	add(1)
+	add(3)
+	fmt.Println(staticArray)
 }
