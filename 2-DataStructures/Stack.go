7ae2e3d3f2a058e6895c32d49a5c975c7ae8b1dc
4m4z1gh <aitbaha48@gmail.com>
2024-07-30 16:43:46 +0300

    --

--- 2-DataStructures/Stack.go
+++ 2-DataStructures/Stack.go
@@ -1,1 +1,14 @@
 package datastructures
+
+var stack []interface{}
+
+func Push(item interface{}) {
+	stack = append(stack, item)
+}
+
+func Pop() interface{} {
+	lastIndex := len(stack) - 1
+	item := stack[lastIndex]
+	stack = stack[:lastIndex-2]
+	return item
+}
