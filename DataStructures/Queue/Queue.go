406334cdd9f1a7789aa5d88c511fcbd2097fb53a
4m4z1gh <aitbaha48@gmail.com>
2024-07-30 20:27:23 +0300

    --

--- DataStructures/Queue/Queue.go
+++ DataStructures/Queue/Queue.go
@@ -1,1 +1,21 @@
-package datastructures
+package queue
+
+type Queue struct {
+	items []interface{}
+}
+
+func NewQueue() *Queue {
+	return &Queue{
+		items: make([]interface{}, 0),
+	}
+}
+func (o *Queue) InQueue(item interface{}) {
+	o.items = append([]interface{}{item}, o.items...)
+}
+
+func (o *Queue) DeQueue() interface{} {
+	lastIndex := len(o.items) - 1
+	item := o.items[lastIndex]
+	o.items = o.items[:lastIndex]
+	return item
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	queue "DSA/DataStructures/Queue"
 	stack "DSA/DataStructures/Stack"
 	"fmt"
 )
@@ -14,6 +15,18 @@ func StackTest() {
 	s.Push(3)
 	fmt.Println(s, s.Pop(), s)
 }
+
+func QueueTest() {
+
+	s := queue.NewQueue()
+	s.InQueue(1)
+	s.InQueue(6)
+	s.InQueue(4)
+	s.InQueue(3)
+	fmt.Println(s, s.DeQueue(), s)
+}
+
 func main() {
 	StackTest()
+	QueueTest()
 }
