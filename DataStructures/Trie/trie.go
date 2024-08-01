1b430d327ab6a6139830701bb28b38d6f3646666
4m4z1gh <aitbaha48@gmail.com>
2024-08-01 03:49:35 +0300

    --

--- DataStructures/Trie/trie.go
+++ DataStructures/Trie/trie.go
@@ -12,6 +12,15 @@ func NewTrie(value int) *Node {
 	return &Node{value: value, Left: nil, Right: nil}
 }
 
+func printTrie(node *Node) {
+	if node == nil {
+		return
+	}
+	fmt.Printf("Val : %v\n", node.value)
+	printTrie(node.Left)
+	printTrie(node.Right)
+}
+
 func insert(node *Node, value int) *Node {
 	if node == nil {
 		return &Node{value: value, Left: nil, Right: nil}
@@ -26,19 +35,51 @@ func insert(node *Node, value int) *Node {
 	return node
 
 }
+
+func minValueNode(root *Node) *Node {
+	curr := root
+
+	for curr != nil && curr.Left != nil {
+		curr = curr.Left
+	}
+	return curr
+}
+
+func remove(root *Node, value int) *Node {
+
+	if root == nil {
+		return nil
+	}
+
+	if value > root.value {
+		root.Right = remove(root.Right, value)
+	} else if value < root.value {
+		root.Left = remove(root.Left, value)
+	} else {
+		if root.Left == nil {
+			return root.Right
+		} else if root.Right == nil {
+			return root.Left
+		} else {
+			minNode := minValueNode(root.Right)
+			root.value = minNode.value
+			root.Right = remove(root.Right, minNode.value)
+		}
+	}
+	return root
+
+	return nil
+}
+
 func (o *Node) Insert(value int) {
 
 	insert(o, value)
 }
 
-func printTrie(node *Node) {
-	if node == nil {
-		return
-	}
-	fmt.Printf("Val : %v\n", node.value)
-	printTrie(node.Left)
-	printTrie(node.Right)
+func (o *Node) Remove(value int) {
+	_ = remove(o, value)
 }
+
 func (o *Node) Print() {
 	printTrie(o)
 }
