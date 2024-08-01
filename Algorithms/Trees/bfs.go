219c14f54f5a6ea404142eeee2543775164ac5a6
maruki00 <aitbaha48@gmail.com>
2024-08-01 18:28:21 +0300

    --

--- Algorithms/Trees/bfs.go
+++ Algorithms/Trees/bfs.go
@@ -1,26 +1,55 @@
 package trees
 
 import (
-	queue "DSA/DataStructures/Queue"
 	trie "DSA/DataStructures/Trie"
 	"fmt"
 )
 
-var (
-	q queue.Queue = *queue.NewQueue()
-)
+type Queue struct {
+	items []*trie.Node
+}
+
+func (o *Queue) InQueue(node *trie.Node) {
+	o.items = append([]*trie.Node{node}, o.items...)
+}
+
+func (o *Queue) Items() []*trie.Node {
+	return o.items
+}
+
+func (o *Queue) IsEmpty() bool {
+	return len(o.items) == 0
+}
+
+func (o *Queue) DeQueue() *trie.Node {
+	l := len(o.items) - 1
+	item := o.items[l]
+	o.items = o.items[:l]
+	return item
+
+}
 
 func BFS(root *trie.Node) {
-	q.InQueue(root)
-
+	level := 1
+	q := &Queue{items: make([]*trie.Node, 0)}
+	if root != nil {
+		q.InQueue(root)
+	}
 	for !q.IsEmpty() {
-		tmp := q.DeQueue()
-		fmt.Printf("value : %v\n", tmp.GetValue())
-		if tmp.Left != nil {
-			q.InQueue(tmp.Left)
+		fmt.Println("level ", level)
+
+		for range len(q.Items()) {
+
+			tmp := q.DeQueue()
+			fmt.Printf("value : %v\n", tmp.GetValue())
+
+			if tmp.Left != nil {
+				q.InQueue(tmp.Left)
+			}
+			if tmp.Right != nil {
+				q.InQueue(tmp.Right)
+			}
 		}
-		if tmp.Right != nil {
-			q.InQueue(tmp.Right)
-		}
+		level++
 	}
 }
