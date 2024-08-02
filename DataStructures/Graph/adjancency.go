23ed437390104a3df1a7d5d372b2c0391613c6b0
maruki00 <aitbaha48@gmail.com>
2024-08-03 02:44:29 +0300

    --

--- DataStructures/Graph/adjancency.go
+++ DataStructures/Graph/adjancency.go
@@ -9,3 +9,23 @@ func NewAdjancencyGraph() *AdjancencyGraph {
 		nodes: make(map[int][]int, 0),
 	}
 }
+
+func (this *AdjancencyGraph) Add(from, to int) {
+	if _, ok := this.nodes[from]; ok == false {
+		this.nodes[from] = make([]int, 0)
+	}
+	this.nodes[from] = append(this.nodes[from], to)
+}
+
+func (this *AdjancencyGraph) Delete(node int) {
+	delete(this.nodes, node)
+
+	for _, nodes := range this.nodes {
+		for i, n := range nodes {
+			if n == node {
+				nodes = append(nodes[:i], nodes[i+1:]...)
+			}
+		}
+	}
+
+}
