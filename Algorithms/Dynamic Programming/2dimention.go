0ca422fe04e6626799c035bc6a89c2319a1cfd1b
maruki00 <aitbaha48@gmail.com>
2024-08-03 21:29:36 +0300

    --

--- Algorithms/Dynamic Programming/2dimention.go
+++ Algorithms/Dynamic Programming/2dimention.go
@@ -1,24 +1,19 @@
 package dynamic_programming
 
-import "github.com/rogpeppe/go-internal/cache"
+func memorization_2(r int, c int, rows int, cols int, cache map[[2]int]int) int {
 
-
-
-
-func memorization_2(r,c,rows,cols int cache map[[2]int]int) int {
-	
 	if r == rows || c == cols {
 		return 0
 	}
-	
-	if cache[[2]int{r,c}] > 0 {
-		cache[[2]int{r,c}] 
+
+	if cache[[2]int{r, c}] > 0 {
+		return cache[[2]int{r, c}]
 	}
 
-	if r == rows-1 && c == clos-1{
+	if r == rows-1 && c == cols-1 {
 		return 1
 	}
 
-	cache[[2]int{r,c}] = (memorization_2(r+1, c, rows, cols, cache) + memorization_2(r, c+1, rows, cols, cache)) 
-	return cache[[2]int{r,c}]
-}	
+	cache[[2]int{r, c}] = (memorization_2(r+1, c, rows, cols, cache) + memorization_2(r, c+1, rows, cols, cache))
+	return cache[[2]int{r, c}]
+}
