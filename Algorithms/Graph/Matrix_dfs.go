bbe196a5367f6acd443bc32eadfb631fdf653f44
maruki00 <aitbaha48@gmail.com>
2024-08-03 20:13:18 +0300

    --

---
+++ Algorithms/Graph/Matrix_dfs.go
@@ -0,0 +1,23 @@
+package graph
+
+func dfs(grid [][]int, r, c int, visited map[[2]int]bool) int {
+
+	rows, cols := len(grid), len(grid[0])
+
+	if min(r, c) < 0 || visited[[2]int{r, c}] == true || grid[r][c] == 1 {
+		return 0
+	}
+
+	if r == rows-1 || c == cols-1 {
+		return 1
+	}
+
+	visited[[2]int{r, c}] = true
+	count := 0
+	count += dfs(grid, r-1, c, visited)
+	count += dfs(grid, r+1, c, visited)
+	count += dfs(grid, r, c-1, visited)
+	count += dfs(grid, r, c+1, visited)
+	delete(visited, [2]int{r, c})
+	return count
+}
