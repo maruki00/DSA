b81573b3116ebb4f388bb1eb11875672460cf971
maruki00 <aitbaha48@gmail.com>
2024-08-05 06:31:58 +0300

    --

---
+++ Algorithms/Sorting/quick.go
@@ -0,0 +1,21 @@
+package sorting
+
+func QuickSort(arr []int) []int {
+
+	if len(arr) == 1 {
+		return arr
+	}
+	pivot := arr[len(arr)-1]
+	left := []int{}
+	right := []int{}
+
+	for _, item := range arr[:len(arr)-1] {
+		if item > pivot {
+			right = append(right, item)
+			continue
+		}
+		left = append(left, item)
+	}
+
+	return append(append(left, pivot), right...)
+}
